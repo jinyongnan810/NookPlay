fdd5a844eed3892d5baeb925cb4bf7fbf8a9566f
Kin Yuunan <yuunan.kin@gmail.com>
2026-03-23 22:08:05 +0900

    update: improve immersive player part4(start use realitycomposerpro)

--- NookPlay/Features/Player/ImmersivePlayerView.swift
+++ NookPlay/Features/Player/ImmersivePlayerView.swift
@@ -6,6 +6,7 @@
 //
 
 import RealityKit
+import RealityKitContent
 import SwiftUI
 
 /// The content shown inside the app's immersive playback scene.
@@ -14,28 +15,18 @@ import SwiftUI
 /// automatically. This view only provides the docking region that defines where
 /// the docked player should appear.
 struct ImmersivePlayerView: View {
-    private static let dockingAnchorName = "immersive-player-dock"
-    private static let dockingWidth: Float = 8.5
-    private static let dockingPosition = SIMD3<Float>(0, 2.35, -10.2)
+    private static let environmentRootName = "dark-environment"
 
     @Environment(AppModel.self) private var appModel
     @Environment(\.scenePhase) private var scenePhase
 
     var body: some View {
         RealityView { content in
-            guard content.entities.first(where: { $0.name == Self.dockingAnchorName }) == nil else {
-                return
+            if content.entities.first(where: { $0.name == Self.environmentRootName }) == nil {
+                let environmentRoot = await makeEvironment()
+                environmentRoot.name = Self.environmentRootName
+                content.add(environmentRoot)
             }
-
-            let dockingAnchor = Entity()
-            dockingAnchor.name = Self.dockingAnchorName
-            dockingAnchor.position = Self.dockingPosition
-
-            var dockingRegion = DockingRegionComponent()
-            dockingRegion.width = Self.dockingWidth
-            dockingAnchor.components.set(dockingRegion)
-
-            content.add(dockingAnchor)
         }
         .onAppear {
             appModel.setImmersiveSpacePresented(true)
@@ -46,7 +37,6 @@ struct ImmersivePlayerView: View {
         .onChange(of: scenePhase) { _, newPhase in
             appModel.activePlayerViewModel?.handleScenePhaseChange(newPhase)
         }
-        .preferredSurroundingsEffect(.dark)
         .persistentSystemOverlays(.hidden)
     }
 }
